e2088331fced1e5bbd051b0c1d025e2e38ee7087
Jon Jagger <jon@jaggersoft.com>
2018-08-17 02:09:34 +0100

    use latest compiler

--- docker/install.sh
+++ docker/install.sh
@@ -4,7 +4,7 @@ set -e
 # From frolvlad/alpine-mono
 
 apk add --no-cache --virtual=.build-dependencies wget ca-certificates tar xz
-wget "https://archive.archlinux.org/packages/m/mono/mono-4.8.0.495-1-x86_64.pkg.tar.xz" -O "/tmp/mono.pkg.tar.xz"
+wget "https://archive.archlinux.org/packages/m/mono/mono-5.12.0.260-1-x86_64.pkg.tar.xz" -O "/tmp/mono.pkg.tar.xz"
 tar -xJf "/tmp/mono.pkg.tar.xz"
 cert-sync /etc/ssl/certs/ca-certificates.crt
 apk del .build-dependencies
