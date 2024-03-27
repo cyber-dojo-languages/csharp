db1229f4f8073ed73da8930251f21f63764de30e
Faye <faye@kosli.com>
2024-03-27 16:41:38 +0100

    Update base image and mono version

--- docker/install.sh
+++ docker/install.sh
@@ -6,12 +6,12 @@ apk add --no-cache --virtual=.build-dependencies \
   openssl \
   tar \
   wget \
-  xz
+  zstd
 
 # dist.nuget.org requires TLS 1.2 hence install of openssl above
 wget http://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-wget "https://archive.archlinux.org/packages/m/mono/mono-6.4.0.198-1-x86_64.pkg.tar.xz" -O "/tmp/mono.pkg.tar.xz"
-tar -xJf "/tmp/mono.pkg.tar.xz"
+wget "https://archive.archlinux.org/packages/m/mono/mono-6.12.0.206-1-x86_64.pkg.tar.zst" -O "/tmp/mono.pkg.tar.zst"
+tar -xaf "/tmp/mono.pkg.tar.zst"
 cert-sync /etc/ssl/certs/ca-certificates.crt
 apk del .build-dependencies
 rm /tmp/*
