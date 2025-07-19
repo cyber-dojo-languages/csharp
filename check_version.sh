a8d1e399a230bbfe507b05b08e8b126922dfb30e
JonJagger <jon@kosli.com>
2025-07-19 19:06:16 +0100

    Remove -t flag from 'docker run' command in script as there is no tty inside CI

--- check_version.sh
+++ check_version.sh
@@ -7,7 +7,7 @@ readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly EXPECTED=6.12.0.205
-readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'mcs --version')
+readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'mcs --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
